662b933d1829471d5330b64101cae5bd053faddd
MONIKAvv <monikalonewolf35@gmail.com>
2025-11-12 20:38:50 +0530

    loop

---
+++ src/main/kotlin/Loop.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var i = 1
+//    while(i <= 10){
+//        println("Hello World!")
+//        i++
+//    }
+
+    val num = 5
+    while(i <= 10){
+        println("$num * $i = ${num*i}")
+        i++
+    }
+}
