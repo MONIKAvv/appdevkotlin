276816bea5ea526d392b95049f57017d38aafa46
MONIKAvv <monikalonewolf35@gmail.com>
2025-11-12 21:51:04 +0530

    for each

--- src/main/kotlin/Collections.kt
+++ src/main/kotlin/Collections.kt
@@ -1,2 +1,44 @@
-class Collections {
+fun main(){
+//    list, map, set
+//    collections is a container for multi values objects, strings, number, it is immutuable by default and can use mutuable version as well
+
+//    list -> most used one, stores values in ordered, duplicates are allowed
+
+//    val groceries = listOf("monika", "kumari", "is", "a", "good", "girl")
+//    val groceries = mutableListOf("monika", "kumari", "is", "a", "good", "girl")
+//    groceries.add("Egg")
+//    groceries[1] = "butter"
+//    println(groceries)
+//
+//print(groceries.contains("monika "))
+//print(groceries.size)
+
+
+//    set -> unique values, no order guaranteed, no index
+
+//    val students = setOf("Monika", "light", "L", "L")
+//    println(students)
+////    println(students.elementAt(2))  //inorder to get one specific element
+//    for (std in students){
+//        println(std)
+//    }
+//
+//    students.forEach {
+//        println(it)
+//    }
+
+//    map -> stores values with unique key, access via key not index, key should be unique but values could be same
+
+    val student = mapOf(100 to "monika", 3 to "monika")
+//    println(student[100])
+//    println(student)
+    student.forEach{
+        println(it)
+    }
+    for ((key, value) in student){
+        println(key)
+        println(value)
+    }
+
+
 }
--- src/main/kotlin/Loop.kt
+++ src/main/kotlin/Loop.kt
@@ -6,8 +6,30 @@ fun main(){
 //    }
 
     val num = 5
-    while(i <= 10){
-        println("$num * $i = ${num*i}")
-        i++
+//    while(i <= 10){
+//        println("$num * $i = ${num*i}")
+//        i++
+//    }
+
+    var j = 10
+//    do {
+//        println(j)
+//        j--
+//    }while (j >= 1)
+
+    for (k in 1..100) {
+
+        if(k%2==0){
+//            print(k)
+        }
     }
+//range operator uses
+//    for (l in 100 downTo  1 step 2) println(l)
+
+    for(i in 1..20){
+        if(i == 2 || i ==9)continue
+        else println("iphone $i ")
+        if(i == 18) break
+    }
+
 }
